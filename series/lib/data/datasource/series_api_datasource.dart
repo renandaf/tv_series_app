431b771515c205088ec9f5fac414611cbf2653aa
Renndf <m.renanda21ti@mahasiswa.pcr.ac.id>
2022-12-24 20:50:11 +0700

    Penerapan SSL Pinning

--- series/lib/data/datasource/series_api_datasource.dart
+++ series/lib/data/datasource/series_api_datasource.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:core/core.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
 import 'package:series/data/models/season_detail.dart';
 import 'package:series/data/models/series_detail_model.dart';
 import 'package:series/data/models/series_model.dart';
@@ -24,11 +27,21 @@ class SeriesAPIImpl implements SeriesAPI {
 
   SeriesAPIImpl({required this.client});
 
+  Future<SecurityContext> get globalContext async {
+    final sslCert = await rootBundle.load('certificates/certificates.pem');
+    SecurityContext securityContext = SecurityContext(withTrustedRoots: false);
+    securityContext.setTrustedCertificatesBytes(sslCert.buffer.asInt8List());
+    return securityContext;
+  }
+
   @override
   Future<SeasonDetail> getSeasonDetail(int id, int season) async {
+    HttpClient client = HttpClient(context: await globalContext);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => false;
+    IOClient ioClient = IOClient(client);
     final response =
-        await client.get(Uri.parse('$baseUrl/tv/$id/season/$season?$apiKey'));
-
+        await ioClient.get(Uri.parse('$baseUrl/tv/$id/season/$season?$apiKey'));
     if (response.statusCode == 200) {
       return SeasonDetail.fromJson(json.decode(response.body));
     } else {
@@ -38,9 +51,12 @@ class SeriesAPIImpl implements SeriesAPI {
 
   @override
   Future<List<SeriesModel>> getOnAirSeries() async {
+    HttpClient client = HttpClient(context: await globalContext);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => false;
+    IOClient ioClient = IOClient(client);
     final response =
-        await client.get(Uri.parse('$baseUrl/tv/on_the_air?$apiKey'));
-
+        await ioClient.get(Uri.parse('$baseUrl/tv/on_the_air?$apiKey'));
     if (response.statusCode == 200) {
       return SeriesResponse.fromJson(json.decode(response.body)).seriesList;
     } else {
@@ -50,8 +66,11 @@ class SeriesAPIImpl implements SeriesAPI {
 
   @override
   Future<SeriesDetail> getSeriesDetail(int id) async {
-    final response = await client.get(Uri.parse('$baseUrl/tv/$id?$apiKey'));
-
+    HttpClient client = HttpClient(context: await globalContext);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => false;
+    IOClient ioClient = IOClient(client);
+    final response = await ioClient.get(Uri.parse('$baseUrl/tv/$id?$apiKey'));
     if (response.statusCode == 200) {
       return SeriesDetail.fromJson(json.decode(response.body));
     } else {
@@ -61,9 +80,12 @@ class SeriesAPIImpl implements SeriesAPI {
 
   @override
   Future<List<SeriesModel>> getSeriesRecommendations(int id) async {
-    final response =
-        await client.get(Uri.parse('$baseUrl/tv/$id/recommendations?$apiKey'));
-
+    HttpClient client = HttpClient(context: await globalContext);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => false;
+    IOClient ioClient = IOClient(client);
+    final response = await ioClient
+        .get(Uri.parse('$baseUrl/tv/$id/recommendations?$apiKey'));
     if (response.statusCode == 200) {
       return SeriesResponse.fromJson(json.decode(response.body)).seriesList;
     } else {
@@ -73,8 +95,12 @@ class SeriesAPIImpl implements SeriesAPI {
 
   @override
   Future<List<SeriesModel>> getPopularSeries() async {
-    final response = await client.get(Uri.parse('$baseUrl/tv/popular?$apiKey'));
-
+    HttpClient client = HttpClient(context: await globalContext);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => false;
+    IOClient ioClient = IOClient(client);
+    final response =
+        await ioClient.get(Uri.parse('$baseUrl/tv/popular?$apiKey'));
     if (response.statusCode == 200) {
       return SeriesResponse.fromJson(json.decode(response.body)).seriesList;
     } else {
@@ -84,9 +110,12 @@ class SeriesAPIImpl implements SeriesAPI {
 
   @override
   Future<List<SeriesModel>> getTopRatedSeries() async {
+    HttpClient client = HttpClient(context: await globalContext);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => false;
+    IOClient ioClient = IOClient(client);
     final response =
-        await client.get(Uri.parse('$baseUrl/tv/top_rated?$apiKey'));
-
+        await ioClient.get(Uri.parse('$baseUrl/tv/top_rated?$apiKey'));
     if (response.statusCode == 200) {
       return SeriesResponse.fromJson(json.decode(response.body)).seriesList;
     } else {
@@ -96,9 +125,12 @@ class SeriesAPIImpl implements SeriesAPI {
 
   @override
   Future<List<SeriesModel>> searchSeries(String query) async {
-    final response =
-        await client.get(Uri.parse('$baseUrl/search/tv?$apiKey&query=$query'));
-
+    HttpClient client = HttpClient(context: await globalContext);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => false;
+    IOClient ioClient = IOClient(client);
+    final response = await ioClient
+        .get(Uri.parse('$baseUrl/search/tv?$apiKey&query=$query'));
     if (response.statusCode == 200) {
       return SeriesResponse.fromJson(json.decode(response.body)).seriesList;
     } else {
